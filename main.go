89ca6e08b0fe6933e436c5c6b01b03b4471f7599
Simon Kienzler <skienzler@inovex.de>
2019-03-18 10:42:59 +0100

    fix: Reference template files with relative path

--- main.go
+++ main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+
 	t := time.Now()
 	time := t.Format("02.01.2006, 15:04:05 Uhr")
 
-	tmpl := template.Must(template.ParseFiles("./templates/index.html"))
+	// Working Directory
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Template
+	tmpl := template.Must(template.ParseFiles(wd + "/templates/index.html"))
 
 	data := make(map[string]string)
 	data["pageTitle"] = "Jenkins X Test"
